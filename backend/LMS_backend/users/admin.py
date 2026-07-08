868bd65218149c32806cd253a662b50f1feb40a3
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-08 22:03:50 +0530

    Customize how the User model appears in Django Admin

--- backend/LMS_backend/users/admin.py
+++ backend/LMS_backend/users/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Customize how the User model appears in Django Admin
+    ordering = ("id",)
