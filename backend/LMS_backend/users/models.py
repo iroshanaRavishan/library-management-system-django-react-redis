81bb66d973d68a0a7bb67223e5c3c0330c5acfca
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-08 22:00:30 +0530

    add Role class to User model for user role management

--- backend/LMS_backend/users/models.py
+++ backend/LMS_backend/users/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        LIBRARIAN = "LIBRARIAN", "Librarian"
+        MEMBER = "MEMBER", "Member"
+
     email = models.EmailField(
         unique=True, #Every user must have a unique email.
         help_text="Each user must have a unique email address."
