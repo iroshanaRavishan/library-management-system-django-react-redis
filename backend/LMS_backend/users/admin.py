3fea7949dd59bf3eaa569021936022a829d22ab2
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-08 22:04:24 +0530

    Columns displayed in the Django Admin user list

--- backend/LMS_backend/users/admin.py
+++ backend/LMS_backend/users/admin.py
@@ -5,4 +5,16 @@ from .models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     # Customize how the User model appears in Django Admin
+
+    # Columns displayed in the Django Admin user list.
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "full_name",
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
     ordering = ("id",)
