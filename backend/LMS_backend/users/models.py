2883f25cf9f8233b284c4222be56b7739dff023a
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-08 21:58:35 +0530

    add User model with email, full name, and phone number fields

--- backend/LMS_backend/users/models.py
+++ backend/LMS_backend/users/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(
+        unique=True, #Every user must have a unique email.
+        help_text="Each user must have a unique email address."
+    )
+
+    full_name = models.CharField(
+        max_length=150
+    )
+
