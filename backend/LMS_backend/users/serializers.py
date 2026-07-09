8f7f4a0b00a62477e82c8bf55e469b9a8fbfef5b
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-09 22:01:20 +0530

    Used for reading user information

--- backend/LMS_backend/users/serializers.py
+++ backend/LMS_backend/users/serializers.py
@@ -2,3 +2,17 @@ from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 
 from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    # Used for reading user information
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "full_name",
+            "phone_number",
+            "role",
+        )
