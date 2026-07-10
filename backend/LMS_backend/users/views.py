38a39a9a182bf31cc8effe13626b37520db4810d
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-10 21:09:47 +0530

    Implement UserListView to list all users with authentication

--- backend/LMS_backend/users/views.py
+++ backend/LMS_backend/users/views.py
@@ -3,3 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import User
 from .serializers import UserSerializer
+
+class UserListView(generics.ListAPIView):
+    # List all users.
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
