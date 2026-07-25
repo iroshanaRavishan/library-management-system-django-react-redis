9ce215165722214146c907c16d4fe9df7cc7d627
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-25 21:33:40 +0530

    Add missing import statements for APIView and Response in views.py

--- backend/LMS_backend/users/views.py
+++ backend/LMS_backend/users/views.py
@@ -9,6 +9,8 @@ from .serializers import (
 )
 
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class UserListView(generics.ListAPIView):
