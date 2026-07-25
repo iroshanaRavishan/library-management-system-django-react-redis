c034274cb3c4577bb7fcdd98a7a566cf643c286c
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-25 21:34:47 +0530

    Add missing import statements for status and RefreshToken

--- backend/LMS_backend/users/views.py
+++ backend/LMS_backend/users/views.py
@@ -1,5 +1,3 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
 from .models import User
 from .serializers import (
@@ -15,6 +13,8 @@ from rest_framework.permissions import (
     IsAuthenticated,
     AllowAny,
 )
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserListView(generics.ListAPIView):
