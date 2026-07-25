3b456b2d4bbd762636e1d0279b632dfb85e8d3d0
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-25 21:34:11 +0530

    permission imports in views.py for consistency

--- backend/LMS_backend/users/views.py
+++ backend/LMS_backend/users/views.py
@@ -11,6 +11,10 @@ from .serializers import (
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny,
+)
 
 
 class UserListView(generics.ListAPIView):
