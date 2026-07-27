2fdf450dee04539a095bd45b21e8a40cdf8916c6
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-27 22:42:58 +0530

    Implement GET method in ProfileView to return logged-in user's profile

--- backend/LMS_backend/users/views.py
+++ backend/LMS_backend/users/views.py
@@ -75,3 +75,6 @@ class ProfileView(APIView):
     # Return logged-in user's profile.
 
     permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        return Response(status=status.HTTP_200_OK)
