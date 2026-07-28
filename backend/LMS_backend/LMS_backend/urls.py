bb8d328ab4b118050b1f4b08630a195af23a4b4e
IroshanaRavishan <bandarairoshana@gmail.com>
2026-07-28 17:40:49 +0530

    Add TokenRefreshView import to urls.py for JWT token management

--- backend/LMS_backend/LMS_backend/urls.py
+++ backend/LMS_backend/LMS_backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
