87de1e2c37e021e0b62579e95f797d9021fe2f4a
IroshanaRavishan <bandarairoshana@gmail.com>
2026-08-06 10:19:03 +0530

    Add Publisher model to store publisher information

--- backend/LMS_backend/books/models.py
+++ backend/LMS_backend/books/models.py
@@ -19,3 +19,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Publisher(models.Model):
+    # Stores publisher information.
+
+    name = models.CharField(max_length=150, unique=True)
