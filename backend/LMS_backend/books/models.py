4e415ec1355060ca5714c06ee2684a6b15d1c129
IroshanaRavishan <bandarairoshana@gmail.com>
2026-08-06 10:22:26 +0530

    Add __str__ method to Publisher model for better string representation

--- backend/LMS_backend/books/models.py
+++ backend/LMS_backend/books/models.py
@@ -26,3 +26,6 @@ class Publisher(models.Model):
 
     name = models.CharField(max_length=150, unique=True)
     address = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
