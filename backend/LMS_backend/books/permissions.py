14f23ddb2778df37cb1f9383e3d783e2510546d9
IroshanaRavishan <bandarairoshana@gmail.com>
2026-08-17 18:11:55 +0530

    Refactor has_permission method in IsLibrarianOrReadOnly class for clarity

--- backend/LMS_backend/books/permissions.py
+++ backend/LMS_backend/books/permissions.py
@@ -5,3 +5,9 @@ class IsLibrarianOrReadOnly(permissions.BasePermission):
     # Allows anyone who is authenticated to read data.
     # Only librarians can create, update, or delete data.
 
+
+    def has_permission(self, request, view):
+
+        # GET, HEAD and OPTIONS are read-only operations.
+        if request.method in permissions.SAFE_METHODS:
+            return True
