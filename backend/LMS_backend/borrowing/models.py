80d50f067598952cfc2ccd871a9be7de8894211b
iroshanaRavishan <bandarairoshana@gmail.com>
2026-09-04 22:03:41 +0530

    Add docstring to BorrowTransaction model for clarity

--- backend/LMS_backend/borrowing/models.py
+++ backend/LMS_backend/borrowing/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.conf import settings
 
 from books.models import Book
+
+
+class BorrowTransaction(models.Model):
+    """
+    Represents a book borrowing transaction.
+
+    A transaction is created when a member borrows a book.
+    When the book is returned, returned_at is populated.
+    """
