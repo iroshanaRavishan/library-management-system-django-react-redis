d91da9345021a55dfa7739440d19e51194cc77d3
iroshanaRavishan <bandarairoshana@gmail.com>
2026-09-06 21:59:53 +0530

    Add borrow_book function to handle book borrowing process

--- backend/LMS_backend/borrowing/services.py
+++ backend/LMS_backend/borrowing/services.py
@@ -1,2 +1,7 @@
 from django.db import transaction
 from django.utils import timezone
+
+def borrow_book(user, book_id, due_date):
+    """
+    Handles the complete business process of borrowing a book.
+    """
